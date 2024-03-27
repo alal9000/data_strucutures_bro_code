358433c69f8e79e3280cb3007efed9c80b484460
Aaron Lal <lucidlear@gmail.com>
2024-03-28 09:44:38 +1100

    recursion

--- src/Main.java
+++ src/Main.java
@@ -2,30 +2,37 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[] array = {9, 1, 8, 2, 7, 3, 6, 5, 4 };
+        
+//        walk(5);
+//        System.out.println(factorial(7));3
+        System.out.println(power(2, 8));
 
-        insertionSort(array);
-
-        for (int i : array) {
-            System.out.print(i + " ");
-
-        }
-
+        
     }
 
-    private static void insertionSort(int[] array) {
-
-        for (int i = 1; i < array.length; i++) {
-            int temp = array[i];
-            int j = i - 1;
-
-            while (j >= 0 && array[j] > temp) {
-                array[j + 1] = array[j];
-                j--;
-            }
-            array[j + 1] = temp;
-
-        }
+    private static int power(int base, int exponent) {
+        if (exponent < 1) return 1; // base case
+        return base * power(base, exponent - 1); // recursive case
     }
+
+//    private static int factorial(int num) {
+//        if (num < 1) return 1; // base case
+//        return num * factorial(num -1); // recursive case
+//    }
+
+//    private static void walk(int steps) {
+//        for (int i = 0; i < steps; i++) {
+//            System.out.println("You take a step!");
+//
+//        }
+//    }
+
+//    private static void walk(int steps) {
+//        if (steps < 1) return; // base case
+//        System.out.println("You take a step");
+//        walk(steps -1); // recursive case
+//
+//    }
+
 
 }
