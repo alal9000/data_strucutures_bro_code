cb265d607accff9664651b8a29da52b9df56843a
Aaron Lal <lucidlear@gmail.com>
2024-04-03 07:56:17 +1100

    recursion v2

--- src/Main.java
+++ src/Main.java
@@ -2,37 +2,28 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        
-//        walk(5);
-//        System.out.println(factorial(7));3
-        System.out.println(power(2, 8));
-
-        
+        System.out.println(power(2, 2));
     }
 
     private static int power(int base, int exponent) {
-        if (exponent < 1) return 1; // base case
-        return base * power(base, exponent - 1); // recursive case
+
+        if (exponent < 1) return 1; //base case
+
+        return base * power(base, exponent - 1); //recursive case
     }
 
-//    private static int factorial(int num) {
-//        if (num < 1) return 1; // base case
-//        return num * factorial(num -1); // recursive case
-//    }
+    private static void walk(int steps) {
+        if (steps < 1) return;
+        System.out.println("You walked..");
+    }
 
-//    private static void walk(int steps) {
-//        for (int i = 0; i < steps; i++) {
-//            System.out.println("You take a step!");
-//
-//        }
-//    }
 
-//    private static void walk(int steps) {
-//        if (steps < 1) return; // base case
-//        System.out.println("You take a step");
-//        walk(steps -1); // recursive case
-//
-//    }
+    private static int factorial(int num) {
+        if (num < 1) return 1; // base case
+        return num * factorial(num -1); // recursive case
+    }
+
+
 
 
 }
