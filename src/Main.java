ed1ce8e9f3572e40e5e1744661355dc818fa4e08
Aaron Lal <lucidlear@gmail.com>
2024-06-26 07:07:06 +1000

    binary search tree

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,26 @@
-import adjacencyMatrix.Graph;
-import adjacencyMatrix.Node;
+import binarySearchTree.BinarySearchTree;
+import binarySearchTree.Node;
 
 public class Main {
     public static void main(String[] args) {
+        BinarySearchTree tree = new BinarySearchTree();
 
-        Graph graph = new Graph(5);
+        tree.insert(new Node(5));
+        tree.insert(new Node(1));
+        tree.insert(new Node(9));
+        tree.insert(new Node(2));
+        tree.insert(new Node(7));
+        tree.insert(new Node(3));
+        tree.insert(new Node(6));
+        tree.insert(new Node(4));
+        tree.insert(new Node(8));
+
+        tree.remove(0);
+        tree.display();
+
+//        System.out.println(tree.search(10));
 
 
-        graph.addNode(new Node('A'));
-        graph.addNode(new Node('B'));
-        graph.addNode(new Node('C'));
-        graph.addNode(new Node('D'));
-        graph.addNode(new Node('E'));
-
-        graph.addEdge(0, 1);
-        graph.addEdge(1, 2);
-        graph.addEdge(1, 4);
-        graph.addEdge(2, 3);
-        graph.addEdge(2, 4);
-        graph.addEdge(4, 0);
-        graph.addEdge(4, 2);
-
-        graph.print();
-
-        graph.breadthFirstSearch(4);
 
     }
 
