03406ddc1fb5520e6d2eeff0254659e6ef22dea0
Aaron Lal <lucidlear@gmail.com>
2024-07-01 07:22:25 +1000

    calculate execution time

--- src/Main.java
+++ src/Main.java
@@ -6,11 +6,11 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
         long start = System.nanoTime();
 
-        // program start here
+        // program start here..
 
         Thread.sleep(3000);
 
-        // program end here
+        // program end here..
 
         long duration = (System.nanoTime() - start)/1000000;
         System.out.println(duration + "ms");
