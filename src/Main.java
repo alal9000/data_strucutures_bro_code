19b0bfa19d26f060546aca92cf730208bb676bab
Aaron Lal <lucidlear@gmail.com>
2024-07-01 07:26:04 +1000

    last commit

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,35 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        long start = System.nanoTime();
+//        long start = System.nanoTime();
+//
+//        // program start here..
+//
+//        Thread.sleep(3000);
+//
+//        // program end here..
+//
+//        long duration = (System.nanoTime() - start)/1000000;
+//        System.out.println(duration + "ms");
 
-        // program start here..
+        // new up an ArrayList instance called people to hold our Person objects
+        ArrayList<Person> people = new ArrayList<>();
 
-        Thread.sleep(3000);
+        // new up each person object and give it initial values
+        Person person1 = new Person("Aaron", 39, "Mum", "Dad", "tech CEO");
+        Person person2 = new Person("Bob", 40, "Bob", "Stacy", "Plumber");
+        Person person3 = new Person("Mosh", 41, "Belinda", "Stu", "Teacher");
+        Person person4 = new Person("steve", 51, "Mary", "Lyle", "construction worker");
 
-        // program end here..
+        // add each person object to our ArrayList
+        people.add(person1);
+        people.add(person2);
+        people.add(person3);
+        people.add(person4);
 
-        long duration = (System.nanoTime() - start)/1000000;
-        System.out.println(duration + "ms");
+        // call the greet function for each person object in our ArrayList
+        for (Person person:people) {
+            person.greeting();
+        }
     }
 }
